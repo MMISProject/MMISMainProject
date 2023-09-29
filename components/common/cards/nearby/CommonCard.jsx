23814c27082af3100666ae8d434e273cdf8aabd8
MMISProject <mmistechnologies@gmail.com>
2023-09-29 18:47:36 +0530

    main file

---
+++ components/common/cards/nearby/CommonCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './commoncard.style'
+
+const CommonCard = () => {
+  return (
+    <View>
+      <Text>Make  your cards here .. </Text>
+    </View>
+  )
+}
+
+export default CommonCard
