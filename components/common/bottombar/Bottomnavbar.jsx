fe5fd5bc26c9e92ab0aeba4d1c755dbca39d7f77
MMISProject <mmistechnologies@gmail.com>
2023-10-01 12:17:44 +0530

    i have assigned names to respective person

---
+++ components/common/bottombar/Bottomnavbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './bottomnavbar.style'
+
+const bottomnavbar = () => {
+  return (
+    <View>
+      <Text>Make  your navbar here... </Text>
+    </View>
+  )
+}
+
+export default bottomnavbar;
---
+++ components/common/cards/popular/PopularCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './popularcard.style'
+
+const PopularCard = () => {
+  return (
+    <View>
+      <Text>PopularCard</Text>
+    </View>
+  )
+}
+
+export default PopularCard
